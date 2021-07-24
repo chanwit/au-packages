f864ee6263f0bb49a0e380d321b753031a93eb82
chanwit <chanwit@gmail.com>
2021-07-24 17:23:23 +0000

    AU: 1 updated - ekz [skip ci] https://gist.github.com/04b7a680d5866ed9058d925f1746640c/c245c6540394837f5714f935753b486d0e521f5e

--- ekz/tools/chocolateyInstall.ps1
+++ ekz/tools/chocolateyInstall.ps1
@@ -6,8 +6,8 @@ $ErrorActionPreference = 'Stop';
 
 $packageArgs = @{
   packageName            = 'ekz'
-  url64bit               = 'https://github.com/chanwit/ekz/releases/download/v0.7.0/ekz_0.7.0_windows_amd64.zip'
-  checksum64             = 'e4a2963261f7948583641f58c07f470411abe0cf34785d1f7d0bb073be298efa'
+  url64bit               = 'https://github.com/chanwit/ekz/releases/download/v0.8.0/ekz_0.8.0_windows_amd64.zip'
+  checksum64             = '5c0aa942e75db97f9c0925c8b672c52bfaa907233075ca74a5eee340ccaa4e08'
   checksumType64         = 'sha256'
   unzipLocation          = $installDir
 }
